e2713f5c22d86f2415bfa4ff1df95402ff009c31
kao126 <76607249+kao126@users.noreply.github.com>
2022-07-31 15:10:59 +0000

    9b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
     @today_record = @books.where(created_at: Time.now.all_day)
     @yesterday_record = @books.where(created_at: 1.day.ago.all_day)
     @this_week_record = @books.where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)
     @last_week_record = @books.where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)
-    @book = Book.new
   end
 
   def index
@@ -26,6 +26,12 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def daily_posts
+    user = User.find(params[:user_id])
+    @books = user.books.where(created_at: params[:created_at].to_date.all_day)
+    render :daily_posts_form
   end
 
   private
